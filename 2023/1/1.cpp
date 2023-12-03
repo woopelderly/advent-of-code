f788a4b7c4dfeb09eb03ec5ca8b7c7eefb01d786
Joel Shuman <joel.shuman@gentex.com>
2023-12-02 19:58:51 -0500

    Day 1

---
+++ 2023/1/1.cpp
@@ -0,0 +1,28 @@
+#include "concept_utils.h"
+#include "input.h"
+
+#include <algorithm>
+#include <cctype>
+#include <cstdint>
+#include <iostream>
+#include <numeric>
+#include <string>
+
+namespace aoc_1
+{
+    [[nodiscard]] std::uint64_t constexpr decode( concept_utils::Container auto scrambed_data )
+    {
+        return std::accumulate(
+            scrambed_data.begin(), scrambed_data.end(), 0, []( std::uint64_t total, std::string const& input ) {
+                auto const first_digit{ std::find_if( input.begin(), input.end(), []( unsigned char c ) { return std::isdigit( c ); } ) };
+                auto const last_digit{ std::find_if( input.rbegin(), input.rend(), []( unsigned char c ) { return std::isdigit( c ); } ) };
+
+                return total += ( *first_digit - 0x30 ) * 10 + ( *last_digit - 0x30 );
+            } );
+    }
+}
+
+int main()
+{
+    std::cout << aoc_1::decode( aoc_1::input ) << '\n';
+}
---
+++ 2023/1/concept_utils.h
@@ -0,0 +1,50 @@
+#pragma once
+
+#include <concepts>
+#include <iterator>
+
+namespace concept_utils
+{
+    // https://stackoverflow.com/a/70543294
+    template< typename ContainerType_t >
+    concept Container =
+        requires( ContainerType_t a, const ContainerType_t b ) {
+            requires std::regular< ContainerType_t >;
+            requires std::swappable< ContainerType_t >;
+            requires std::destructible< typename ContainerType_t::value_type >;
+            requires std::same_as< typename ContainerType_t::reference, typename ContainerType_t::value_type& >;
+            requires std::same_as< typename ContainerType_t::const_reference, const typename ContainerType_t::value_type& >;
+            requires std::forward_iterator< typename ContainerType_t::iterator >;
+            requires std::forward_iterator< typename ContainerType_t::const_iterator >;
+            requires std::signed_integral< typename ContainerType_t::difference_type >;
+            requires std::same_as< typename ContainerType_t::difference_type, typename std::iterator_traits< typename ContainerType_t::iterator >::difference_type >;
+            requires std::same_as< typename ContainerType_t::difference_type, typename std::iterator_traits< typename ContainerType_t::const_iterator >::difference_type >;
+            {
+                a.begin()
+                } -> std::same_as< typename ContainerType_t::iterator >;
+            {
+                a.end()
+                } -> std::same_as< typename ContainerType_t::iterator >;
+            {
+                b.begin()
+                } -> std::same_as< typename ContainerType_t::const_iterator >;
+            {
+                b.end()
+                } -> std::same_as< typename ContainerType_t::const_iterator >;
+            {
+                a.cbegin()
+                } -> std::same_as< typename ContainerType_t::const_iterator >;
+            {
+                a.cend()
+                } -> std::same_as< typename ContainerType_t::const_iterator >;
+            {
+                a.size()
+                } -> std::same_as< typename ContainerType_t::size_type >;
+            {
+                a.max_size()
+                } -> std::same_as< typename ContainerType_t::size_type >;
+            {
+                a.empty()
+                } -> std::same_as< bool >;
+        };
+}
